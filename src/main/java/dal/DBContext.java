0d0b26e7824d87b579d8f7a54efd0b45ac48c508
HieuLXHE187201 <hieulxhe187201@fpt.edu.vn>
2025-06-19 21:34:00 +0700

    validate

--- src/main/java/dal/DBContext.java
+++ src/main/java/dal/DBContext.java
@@ -7,13 +7,13 @@ import java.sql.SQLException;
 public class DBContext {
 
     // Cách 1: tắt SSL
-     private static final String URL = "jdbc:sqlserver://localhost:1433;"
+     private static final String URL = "jdbc:sqlserver://LAPTOP-SVEKADBK\\HIEUHE187201:1433;"
             + "databaseName=HotelManagement;"
             + "encrypt=true;trustServerCertificate=true;";
 
   
     private static final String USER = "sa";
-    private static final String PASSWORD = "123";
+    private static final String PASSWORD = "qq123123";
 
     public static Connection getConnection() throws SQLException {
         try {
